376f2c5a71e4359e1da9a6ef26c2aa6668bf6e1b
Kamil Stachowicz <stacho688@gmail.com>
2018-01-19 15:26:05 +0100

    Test with provate field

---
+++ testWithoutClass.js
@@ -0,0 +1,16 @@
+//the objective is to write translation from java to js without using JS Classes
+// class Test {
+//     private name = 'Something';
+  
+//     public getName() {
+//       return name;
+//     }
+//   }
+
+function Test() {
+    let name = "Something"
+    
+    }
+    
+
+console.log(Test.name)
